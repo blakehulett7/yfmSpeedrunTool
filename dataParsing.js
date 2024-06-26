4060fea58ee2fee07109766526b05f8ba1deb86c
blakehulett7 <blake.hulett7@gmail.com>
2024-06-25 23:42:13 -0500

    About to isolate the title. Just a bit of string manipulation, nothing crazy!!

--- dataParsing.js
+++ dataParsing.js
@@ -3,4 +3,9 @@ import { readFileSync } from 'node:fs'
 const rawCharactersString = readFileSync('./data/characters.js', 'utf8')
 const rawCharacterList = rawCharactersString.split('\n')
 const trimmedCharacterList = rawCharacterList.slice(1, -3)
-console.log(trimmedCharacterList)
+const regex = /link=(.*)/g
+const characterLinks = []
+for (let character of trimmedCharacterList) {
+  characterLinks.push(character.match(regex))
+}
+console.log(characterLinks)
