fafc78e8f7c43d6918cdd57d54133cc7a3d81f64
blakehulett7 <blake.hulett7@gmail.com>
2024-06-25 23:54:59 -0500

    Lol!!! Regex to the rescue! So powerful

--- dataParsing.js
+++ dataParsing.js
@@ -3,7 +3,7 @@ import { readFileSync } from 'node:fs'
 const rawCharactersString = readFileSync('./data/characters.js', 'utf8')
 const rawCharacterList = rawCharactersString.split('\n')
 const trimmedCharacterList = rawCharacterList.slice(1, -3)
-const regex = /link=(.*)/g
+const regex = /(?<=link=)(.*)/g
 const characterLinks = []
 for (let character of trimmedCharacterList) {
   characterLinks.push(character.match(regex)[0])
