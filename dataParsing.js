3cfa3919bb2739b1a57123cbffff6088841f7b17
blakehulett7 <blake.hulett7@gmail.com>
2024-06-25 23:44:10 -0500

    Quick one!

--- dataParsing.js
+++ dataParsing.js
@@ -6,6 +6,6 @@ const trimmedCharacterList = rawCharacterList.slice(1, -3)
 const regex = /link=(.*)/g
 const characterLinks = []
 for (let character of trimmedCharacterList) {
-  characterLinks.push(character.match(regex))
+  characterLinks.push(character.match(regex)[0])
 }
 console.log(characterLinks)
