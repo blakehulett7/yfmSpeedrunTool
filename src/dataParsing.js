302db3f7f8cf97cc72b1d4e24f36aa14e122ebbe
blakehulett7 <blake.hulett7@gmail.com>
2024-06-27 02:22:49 -0500

    Working late!

--- src/dataParsing.js
+++ src/dataParsing.js
@@ -1,13 +1,17 @@
 import { readFileSync } from 'node:fs'
 
-const rawCharacters = readFileSync('./data/characters.json', 'utf8')
-const rawCharactersJSON = JSON.parse(rawCharacters)
-const rawCharactersString = rawCharactersJSON.parse.wikitext['*']
-const rawCharacterList = rawCharactersString.split('\n')
-const trimmedCharacterList = rawCharacterList.slice(1, -3)
-const regex = /(?<=link=)(.*)/g
-const characterList = []
-for (let character of trimmedCharacterList) {
-  characterList.push(character.match(regex)[0])
+function getCharacters() {
+  const rawCharacters = readFileSync('./data/characters.json', 'utf8')
+  const rawCharactersJSON = JSON.parse(rawCharacters)
+  const rawCharactersString = rawCharactersJSON.parse.wikitext['*']
+  const rawCharacterList = rawCharactersString.split('\n')
+  const trimmedCharacterList = rawCharacterList.slice(1, -3)
+  const regex = /(?<=link=)(.*)/g
+  const characterList = []
+  for (let character of trimmedCharacterList) {
+    characterList.push(character.match(regex)[0])
+  }
+  return characterList
 }
-console.log(characterList)
+
+export { getCharacters }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import { fetchPage } from './dataFetching.js'
-import { writeFile, readFile } from 'node:fs'
+import { getCharacters } from './dataParsing.js'
+import { writeFile, readFile, existsSync } from 'node:fs'
 
 console.log("Christ is King!")
 
 function main() {
-  fetchPage('Portal:Yu-Gi-Oh! Forbidden Memories characters', 'characters')
+  console.log('Welcome to the Forbidden Memories Speedrun Tool!')
+  console.log('Checking for character list...')
+  if (existsSync('./data/characters.json')) {
+    console.log('character list found!')
+  } else {
+    fetchPage('Portal:Yu-Gi-Oh! Forbidden Memories characters', 'characters')
+  }
+  const characterList = getCharacters()
+  for (let character of characterList) {
+    let pathName = character.replaceAll(' ', '_')
+    console.log(`checking for ${character} data...`)
+    if (existsSync(`./data/${pathName}.json`)) {
+      console.log(`${character} data found!`)
+    } else {
+      fetchPage(character, pathName)
+    }
+  }
 }
+
+main()
