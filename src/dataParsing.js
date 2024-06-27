4dee2ac7a65ad6441d73cad58740b7e9540e5f1f
blakehulett7 <blake.hulett7@gmail.com>
2024-06-27 02:33:37 -0500

    Split out a generic parseWikitext function to parse all this json!

--- src/dataParsing.js
+++ src/dataParsing.js
@@ -1,9 +1,13 @@
 import { readFileSync } from 'node:fs'
 
+function parseWikitext(rawJsonString) {
+  return rawJsonString.parse.wikitext['*']
+}
+
 function getCharacters() {
   const rawCharacters = readFileSync('./data/characters.json', 'utf8')
   const rawCharactersJSON = JSON.parse(rawCharacters)
-  const rawCharactersString = rawCharactersJSON.parse.wikitext['*']
+  const rawCharactersString = parseWikitext(rawCharactersJSON)
   const rawCharacterList = rawCharactersString.split('\n')
   const trimmedCharacterList = rawCharacterList.slice(1, -3)
   const regex = /(?<=link=)(.*)/g
