781131473e7d636726be5f8a7c55142b58d8c672
blakehulett7 <blake.hulett7@gmail.com>
2024-07-01 13:09:33 -0500

    Little save right here!

--- src/main.js
+++ src/main.js
@@ -11,22 +11,36 @@ function main() {
   console.log('Checking for character list...')
   if (existsSync('./data/raw/characters.json')) {
     console.log('character list found!')
-  } else {
+} else {
     fetchPage('Portal:Yu-Gi-Oh! Forbidden Memories characters', 'characters')
-  }
+}
   const characterList = getCharacters()
   console.log('Checking for character data...')
   for (let character of characterList) {
     let pathName = character.replaceAll(' ', '_')
     if (existsSync(`./data/raw/${pathName}.json`)) {
       console.log(`${character} data found!`)
-    } else {
+  } else {
       fetchPage(character, pathName)
+  }
+  }
+  const opponents = []
+  for (let character of characterList) {
+    if (buildDropTable(character)) {
+      opponents.push(character)
     }
   }
-  console.log('Checking for drop tables...')
-  const df = buildDropTable(characterList[1])
-  writeDropTable(df, characterList[1])
+  console.log('\nChecking for drop tables...')
+  for (let character of opponents) {
+    let characterPath = character.replaceAll(' ', '_');
+    if (existsSync(`./data/processed/${characterPath}_dropTable.csv`)) {
+      console.log(`${character} drop table found!`)
+  } else {
+      console.log(`Writing ${character} drop table.`)
+      let df = buildDropTable(character)
+      writeDropTable(df, character)
+  }
+  }
 }
 
 main()
