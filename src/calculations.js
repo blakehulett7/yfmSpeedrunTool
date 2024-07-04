3938a7a982ad85be014b28edd823f48d23b8d7e6
blakehulett7 <blake.hulett7@gmail.com>
2024-07-03 20:09:18 -0500

    Living the polars life... Boutta do some serious left joining!!

--- src/calculations.js
+++ src/calculations.js
@@ -5,11 +5,19 @@ function buildFarmTable(championObject) {
   const keys = Object.keys(championObject)
   const m1Df = pl.DataFrame(championObject.m1, {columns: ['card']})
   const opponents = readdirSync('./data/processed/dropTables')
-  let dropTables = []
-  const dt = pl.readCSV(`./data/processed/dropTables/${opponents[0]}`)
-  const result = m1Df.join(dt, {on: 'card', how: 'left'})
-  let sums = result.sum()
-  console.log(sums)
+  for (let opponent of opponents) {
+    let dropTable = pl.readCSV(`./data/processed/dropTables/${opponent}`)
+    let trimmedTable = m1Df.join(dropTable, {on: 'card', how: 'left'})
+    let farmScore = trimmedTable.sum()
+    let title = opponent.replace('_(FMR)_dropTable.csv', '')
+    title = title.replace('_dropTable.csv', '')
+    title = title.replaceAll('_', ' ')
+    farmScore = farmScore.rename({'card': 'Group', 'SaPow': `${title}\nSaPow`, 'BCD': `${title}\nBCD`, 'SaTec': `${title}\nSaTec`})
+    farmScore = farmScore.withColumns(pl.col('Group').replace(null, 'm1'))
+    console.log(farmScore)
+  }
+  const df = pl.DataFrame(['m1'], {columns: ['Group']})
+  console.log(df)
 }
 
 const tthd = JSON.parse(readFileSync('./data/processed/champions/Twin-headed_Thunder_Dragon.json'))
