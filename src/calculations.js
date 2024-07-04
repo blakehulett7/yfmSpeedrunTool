296ba447b75ebdffd29fc922f2adc8a5eccb6fc4
blakehulett7 <blake.hulett7@gmail.com>
2024-07-03 20:42:26 -0500

    So close! Just need to concat these and allow for the first round to just stand

--- src/calculations.js
+++ src/calculations.js
@@ -3,21 +3,23 @@ import { readFileSync, readdirSync } from 'node:fs'
 
 function buildFarmTable(championObject) {
   const keys = Object.keys(championObject)
-  const m1Df = pl.DataFrame(championObject.m1, {columns: ['card']})
-  const opponents = readdirSync('./data/processed/dropTables')
-  for (let opponent of opponents) {
-    let dropTable = pl.readCSV(`./data/processed/dropTables/${opponent}`)
-    let trimmedTable = m1Df.join(dropTable, {on: 'card', how: 'left'})
-    let farmScore = trimmedTable.sum()
-    let title = opponent.replace('_(FMR)_dropTable.csv', '')
-    title = title.replace('_dropTable.csv', '')
-    title = title.replaceAll('_', ' ')
-    farmScore = farmScore.rename({'card': 'Group', 'SaPow': `${title}\nSaPow`, 'BCD': `${title}\nBCD`, 'SaTec': `${title}\nSaTec`})
-    farmScore = farmScore.withColumns(pl.col('Group').replace(null, 'm1'))
-    console.log(farmScore)
+  for (let key of keys) {
+    const groupData = pl.DataFrame(championObject[key], {columns: ['card']})
+    const opponents = readdirSync('./data/processed/dropTables')
+    let farmTable = pl.DataFrame([key], {columns: ['Group']})
+    for (let opponent of opponents) {
+      let dropTable = pl.readCSV(`./data/processed/dropTables/${opponent}`)
+      let trimmedTable = groupData.join(dropTable, {on: 'card', how: 'left'})
+      let farmScore = trimmedTable.sum()
+      let title = opponent.replace('_(FMR)_dropTable.csv', '')
+      title = title.replace('_dropTable.csv', '')
+      title = title.replaceAll('_', ' ')
+      farmScore = farmScore.rename({'card': 'Group', 'SaPow': `${title}\nSaPow`, 'BCD': `${title}\nBCD`, 'SaTec': `${title}\nSaTec`})
+      farmScore = farmScore.withColumns(pl.col('Group').replace(null, key))
+      farmTable = farmTable.join(farmScore, {on: 'Group', how: 'left'})
+    }
+    console.log(farmTable)
   }
-  const df = pl.DataFrame(['m1'], {columns: ['Group']})
-  console.log(df)
 }
 
 const tthd = JSON.parse(readFileSync('./data/processed/champions/Twin-headed_Thunder_Dragon.json'))
