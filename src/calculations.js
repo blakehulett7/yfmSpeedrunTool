f624be73ed0b2c9e7e517c447efa36263a2bff35
blakehulett7 <blake.hulett7@gmail.com>
2024-07-03 20:53:08 -0500

    Got a final farmTable built, but I might reformat the rows and columns

--- src/calculations.js
+++ src/calculations.js
@@ -3,10 +3,11 @@ import { readFileSync, readdirSync } from 'node:fs'
 
 function buildFarmTable(championObject) {
   const keys = Object.keys(championObject)
+  let farmTable = null
   for (let key of keys) {
     const groupData = pl.DataFrame(championObject[key], {columns: ['card']})
     const opponents = readdirSync('./data/processed/dropTables')
-    let farmTable = pl.DataFrame([key], {columns: ['Group']})
+    let farmRow = pl.DataFrame([key], {columns: ['Group']})
     for (let opponent of opponents) {
       let dropTable = pl.readCSV(`./data/processed/dropTables/${opponent}`)
       let trimmedTable = groupData.join(dropTable, {on: 'card', how: 'left'})
@@ -16,10 +17,16 @@ function buildFarmTable(championObject) {
       title = title.replaceAll('_', ' ')
       farmScore = farmScore.rename({'card': 'Group', 'SaPow': `${title}\nSaPow`, 'BCD': `${title}\nBCD`, 'SaTec': `${title}\nSaTec`})
       farmScore = farmScore.withColumns(pl.col('Group').replace(null, key))
-      farmTable = farmTable.join(farmScore, {on: 'Group', how: 'left'})
+      farmRow = farmRow.join(farmScore, {on: 'Group', how: 'left'})
     }
-    console.log(farmTable)
+    if (farmTable != null) {
+      farmTable = pl.concat([farmTable, farmRow])
+  } else {
+      farmTable = farmRow
+    }
   }
+  console.log(farmTable)
+  console.log(farmTable.sum())
 }
 
 const tthd = JSON.parse(readFileSync('./data/processed/champions/Twin-headed_Thunder_Dragon.json'))
